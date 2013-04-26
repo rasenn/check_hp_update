41f7e377f7937e082df0d0f647b46848f7a9c2b7
rasenn <i.know.defeat@gmail.com>
2013-04-26 22:50:22 +0900

    for commit heroku

--- app/models/url.rb
+++ app/models/url.rb
@@ -10,6 +10,16 @@ class Url < ActiveRecord::Base
     end
   end
 
+  def self.update_titles
+    process_html = ProcessHTML.new
+    urls = self.find(:all)
+    urls.each do |url|
+      title,source = process_html.get_title_and_sanitize_source(url.url)
+      url[:title] = title 
+      url.save
+    end
+  end
+
   # 更新をチェックし、last_crawlを更新する
   # 内容が更新されていればURLのsource,last_updateを更新する。
   def check_update
@@ -28,7 +38,7 @@ class Url < ActiveRecord::Base
   ## 登録数の多い件数を表示する。
   def Url.get_popular_urls
     return self.
-      select("count(*) as count,check_histories.url_id,urls.url,urls.title ").
+      select("count(*),urls.id,urls.url,urls.title ").
       joins("INNER JOIN check_histories on check_histories.url_id=urls.id").
       group("urls.id").
       order("count(*) desc")
