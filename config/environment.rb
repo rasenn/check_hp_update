ea156481b68def752ca9077223b267dc2a3fb367
rasenn <i.know.defeat@gmail.com>
2013-05-27 23:33:26 +0900

    rechange commit

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MangaChecker::Application.initialize!
 
-
-:::ruby
+# Added by @RASENN
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
