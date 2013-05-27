78762bd28502a150f770d61297cd8d85b6564870
rasenn <i.know.defeat@gmail.com>
2013-05-28 00:05:41 +0900

    add change mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ MangaChecker::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # added by @RASENN
+  config.action_mailer.default_url_options = { :host => 'heroku.com' }
+
 end
